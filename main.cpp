e8651f52206f940fb78fb69a75cdb4e2c494f780
FlyMen228 <flymen228@mail.ru>
2023-07-18 18:45:20 +0500

    Release

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
 #include <vector>
-#include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include <stdio.h>
 
 template <typename T>
 void BubbleSort(std::vector<T>& array);
 
+std::vector<int> GenerateRandomIntVector(); 
+
 int main(int argc, char* argv[]) {
-    std::ifstream reader(".\\input.txt");
-
-    std::vector<int> array;
-
-    while (!reader.eof()) {
-        int number;
-        reader >> number;
-
-        array.emplace_back(number);
-    }
+    auto array = GenerateRandomIntVector();
 
     BubbleSort(array);
 
     for (const auto& elem : array) {
-        std::printf("%d ", elem);
+        printf("%d ", elem);
     }
 
     return 0;
 }
 
+std::vector<int> GenerateRandomIntVector() {
+    std::vector<int> array;
+
+    for (auto i = 1; i <= 10000; ++i) {
+        srand(time(0) * i);
+        
+        auto number = (rand() % 1000 + 1) * (rand() % 3 - 1);
+
+        array.emplace_back(number);
+    }
+
+    return array;
+}
+
 template <typename T>
 void BubbleSort(std::vector<T>& array) {
+    for (std::size_t i = 0; i < array.size() - 1; ++i) {
+        bool break_point = false;
 
+        for (std::size_t j = 0; j < array.size() - i - 1; ++j) {
+            if (array[j] > array[j+1]) {
+                std::swap(array[j], array[j+1]);
+                
+                break_point = true;
+            }
+        }
+
+        if (!break_point) {
+            break;
+        }
+    }
 }
